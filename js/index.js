ff8cb988132045367336168e8b8ee816bb06804f
dreamer_dk <dreamer_dk@mail.ru>
2025-07-14 18:46:06 +0300

    slider

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".slider__pagination button")
+const elements = document.querySelectorAll(".element")
+const nav = document.querySelectorAll("nav a")
+
+nav.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault()
+        const id = link.getAttribute("href")
+        const element = document.querySelector(id)
+        element.scrollIntoView({ behavior: "smooth" })
+    })
+})
+
+buttons.forEach((button, n) => {
+    button.addEventListener("click", () => {
+        for (let element of elements) {
+            element.style.left = -130 * n + "%"
+        }
+        for (let button of buttons) {
+            button.style.opacity = 0.5
+        }
+        button.style.opacity = 1
+    })
+})
